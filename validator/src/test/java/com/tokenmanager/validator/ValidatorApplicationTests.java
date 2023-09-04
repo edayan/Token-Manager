13ed36fff362eb2423304a74a0d01a7411baed67
Edayan <saju.edayan@blu-beyond.com>
2023-09-04 21:46:38 +0200

    Adding validator spring boot app

---
+++ validator/src/test/java/com/tokenmanager/validator/ValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tokenmanager.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
